2814161855d47176f6102b6e25fb6661fa40aa2d
Chiaki Mizuochi <m.chiaki.515@gmail.com>
2018-11-26 13:18:43 +0900

    オブジェクトをクリックすると縮小

---
+++ Assets/RayTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayTest : MonoBehaviour {
+
+    GameObject clickedGameObject;
+    Vector2 smlSize;
+
+    void Start(){
+        smlSize = new Vector2(0.5f, 0.5f);
+    }
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            clickedGameObject = null;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit2d = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+
+            if (hit2d)
+            {
+                clickedGameObject = hit2d.transform.gameObject;
+                hit2d.transform.localScale = smlSize;
+            }
+
+            Debug.Log(clickedGameObject);
+        }
+    }
+
+}
